dcd8a290c563c8ba73cdd0807bdbfbf0bd47bf55
Neil Mansilla <neil@mansilla.com>
2014-11-09 15:21:32 -0800

    Added `users.info` and `channels.join`

--- lib/slack/client.rb
+++ lib/slack/client.rb
@@ -1,7 +1,7 @@
 module Slack
   class Client < API
-    METHODS = %w(users.list channels.history channels.mark
-                channels.list files.upload files.list
+    METHODS = %w(users.info users.list channels.history channels.join
+                channels.mark channels.list files.upload files.list
                 im.history im.list groups.history groups.list
                 search.all search.files search.messages
                 chat.postMessage auth.test)
