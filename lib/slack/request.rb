703e54858b73ae9942d7e66e102dda48df0b56b1
aki017 <aki@aki017.info>
2015-05-06 10:08:57 +0900

    Fix Slack::Request#request not to break arguments

--- lib/slack/request.rb
+++ lib/slack/request.rb
@@ -25,7 +25,7 @@ module Slack
 
     # Perform an HTTP request
     def request(method, path, options)
-      options.merge!(token: token)
+      options = options.merge(token: token)
       response = connection.send(method) do |request|
         case method
         when :get, :delete
